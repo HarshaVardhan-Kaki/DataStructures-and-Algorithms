019cb493f20112f75fa178b80fb9e2a85d48e415
Harsha Vardhan <harshavardhan@Harshas-MacBook-Air.local>
2020-08-27 20:59:00 -0700

    remove duplicates from sorted array

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,31 @@
+# leetcode
+
+# O(n) time and O(1) space
+# n is length of array
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+
+# even though this solution returns correct length..we need to return array 
+# that brings unique elements to the front
+# class Solution:
+#     def removeDuplicates(self, nums):
+#         if len(nums) == 1:
+#             return 1
+#         unique_elements = 1
+#         previous_seen = nums[0]
+#         for i in range(1,len(nums)):
+#             if nums[i] == previous_seen:
+#                 continue
+#             else:
+#                 previous_seen = nums[i]
+#                 unique_elements += 1
+#         return unique_elements
